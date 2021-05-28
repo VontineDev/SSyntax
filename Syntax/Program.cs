e705f112f886e5f7cc43be1c698d7b43d33181f5
VontineDev <57723128+VontineDev@users.noreply.github.com>
2021-05-28 11:02:00 +0900

    Syntax.cs Upload

---
+++ Syntax/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
