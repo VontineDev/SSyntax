93bb057b61979d7af20817a729fe79057a909add
VontineDev <57723128+VontineDev@users.noreply.github.com>
2021-06-11 18:23:36 +0900

    키넥트용 샘플코드 업데이트

---
+++ Kinect Sample Source/p2_Depth/p2_Depth/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Kinect;
+
+namespace p2_Depth
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int MapDepthToByte = 8000 / 256;          // 나중에
+
+        private KinectSensor _sensor = null;
+        private DepthFrameReader _depthFrameReader = null;
+        private WriteableBitmap _bitmap;
+        private FrameDescription _description = null;
+        private ushort[] _buffer = null;
+        private int _stride;
+
+        public ImageSource ImageSource
+        {
+            get
+            {
+                return _bitmap;
+            }
+        }
+
+        public MainWindow()
+        {
+            _sensor = KinectSensor.GetDefault();
+            _depthFrameReader = _sensor.DepthFrameSource.OpenReader();
+            _depthFrameReader.FrameArrived += _depthFrameReader_FrameArrived;
+
+            _description = _sensor.DepthFrameSource.FrameDescription;
+            _bitmap = new WriteableBitmap(_description.Width, _description.Height, 96.0, 96.0, PixelFormats.Gray16, null);
+            _buffer = new ushort[_description.LengthInPixels];
+            // 픽셀수 * 한픽셀에 몇바이트인지 그다음 stride는 다음줄로 넘어가기 위한 변수
+            _stride = (int)(_description.Width * _description.BytesPerPixel);
+            _sensor.Open();
+
+            this.DataContext = this;
+
+            InitializeComponent();
+        }
+
+        private void _depthFrameReader_FrameArrived(object sender, DepthFrameArrivedEventArgs e)
+        {
+            using (DepthFrame depthFrame = e.FrameReference.AcquireFrame())
+            {
+                if (depthFrame != null)
+                {
+                    depthFrame.CopyFrameDataToArray(_buffer);
+
+                    for (int i = 0; i < _buffer.Length; i++)
+                    {
+                        //8미터가 거의 최대인식거리 이므로 8000으로 나눠줬다
+                        _buffer[i] = (ushort)(_buffer[i] * 65535 / 8000);
+                    }
+
+                    _bitmap.WritePixels(new Int32Rect(0, 0, _bitmap.PixelWidth, _bitmap.PixelHeight), _buffer, _stride, 0);
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_depthFrameReader != null)
+            {
+                _depthFrameReader.Dispose();
+                _depthFrameReader = null;
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+                _sensor = null;
+            }
+        }
+    }
+}
---
+++ KinectPractice/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+//using System.Windows.Shapes; //Shape는 키넥트와 이름이 많이 겹치므로 없애준다
+
+using Microsoft.Kinect;
+
+
+
+namespace KinectPractice
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor _sensor = null;
+        private ColorFrameReader _colorFrameReader = null;
+        private WriteableBitmap _bitmap = null;
+
+        public ImageSource ImageSource
+        {
+            get { return _bitmap; }
+        }
+
+        public MainWindow()
+        {
+            _sensor = KinectSensor.GetDefault(); //키넥트 센서를 가져온다
+            _colorFrameReader = _sensor.ColorFrameSource.OpenReader();
+            //Kinect 2 는 1920*1080으로 가져오는데 이 이미지의 크기를 모른다고 가정하고, 기본 정보를 가져온다.
+            FrameDescription description = _sensor.ColorFrameSource.CreateFrameDescription(ColorImageFormat.Bgra);
+            //비트맵할당
+            _bitmap = new WriteableBitmap(description.Width, description.Height, 96, 96, PixelFormats.Bgra32, null);
+
+            _sensor.Open(); //센서켜기
+
+            _colorFrameReader.FrameArrived += _colorFrameReader_FrameArrived;
+
+            this.DataContext = this;
+
+            InitializeComponent();
+        }
+
+        private void _colorFrameReader_FrameArrived(object sender, ColorFrameArrivedEventArgs e)
+        {
+            using (ColorFrame colorFrame = e.FrameReference.AcquireFrame())
+            {
+                if (colorFrame != null)
+                {
+                    FrameDescription des = colorFrame.FrameDescription;
+                    using (KinectBuffer buffer = colorFrame.LockRawImageBuffer())
+                    {
+                        _bitmap.Lock();
+                        colorFrame.CopyConvertedFrameDataToIntPtr(_bitmap.BackBuffer,
+                            (uint)((des.Width) * (des.Height) * 4), ColorImageFormat.Bgra);
+
+                        _bitmap.AddDirtyRect(new Int32Rect(0, 0, 1920, 1080));
+
+                        _bitmap.Unlock();
+
+                        //Name = "imgControl" 하고같이 쓸 수 있으나 잘 안쓰는 방식
+                        //imgControl.Source = _bitmap;
+
+
+                    }
+                }
+            }
+        }
+    }
+}
